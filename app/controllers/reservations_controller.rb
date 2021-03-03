ccd57197e6282857f2df640294217f1e1af44d13
Qiyun Dai <pianoquin@gmail.com>
2021-03-02 20:36:20 -0600

    implemented #index #show #new and #create for reservations

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,37 @@
+class ReservationsController < ApplicationController
+    def index
+        @reservations = Reservation.all
+    end
+
+    def show
+    end
+
+    def new
+        @reservation = Reservation.new
+    end
+
+    def create
+        @equipment = Equipment.find(params[:equipment_id])
+        @reservation = Reservation.new(reservation_params)
+        @reservation.user = current_user
+        @reservation.equipment = @equipment
+    end
+
+    private
+
+    def reservation_params
+        params.require(:reservation).permit[:start_date, :end_date, :status]
+    end
+
+    def set_reservation
+        @reservation = Reservation.find(params[:id])
+    end
+
+    def save_show_reservation
+        if @reservation.save
+            redirect_to reservation_path(@reservation)
+        else
+            render :new
+        end 
+    end
+end
