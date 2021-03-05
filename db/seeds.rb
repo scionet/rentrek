66ada451276ef723f16dcaf588578968790c8f95
SLim <sam@samlim.com>
2021-03-04 20:32:36 -0500

    seeded 5 equipment rentals

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def seed_equipment
+  puts 'Cleaning database...'
+  Equipment.delete_all
+
+  user = User.new(email: "sam@samlim.com", password: "password123")
+  category = Category.new(name: "Outdoor Equipment")
+
+  user.save
+  category.save
+
+  Equipment.create!(name: "Coleman Camping Tent", user_id: user.id, category_id: category.id)
+  Equipment.create!(name: "Full Suspension Mountain Bikes - Adult", user_id: user.id, category_id: category.id)
+  Equipment.create!(name: "Inflatable Kayaks - Single", user_id: user.id, category_id: category.id)
+  Equipment.create!(name: "Snowshoes - Kids", user_id: user.id, category_id: category.id)
+  Equipment.create!(name: "Snowboard - Adult", user_id: user.id, category_id: category.id)
+  puts Equipment.all
+end
+return seed_equipment
