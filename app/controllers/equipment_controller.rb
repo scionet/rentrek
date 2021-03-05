a0b4c5305c701f291b57cff75cbbf2ca97c4dd85
Qiyun Dai <pianoquin@gmail.com>
2021-03-04 21:25:15 -0600

    added pundit to create and show

--- app/controllers/equipment_controller.rb
+++ app/controllers/equipment_controller.rb
@@ -1,22 +1,29 @@
 class EquipmentController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
   before_action :set_equipment, only: %i[show edit update destroy]
-  after_action :save_show_equipment, only: %i[create update]
 
   def index
-    @equipment = Equipment.all
+    @equipment = policy_scope(Equipment).order(created_at: :desc)
   end
 
   def show
+    authorize @equipment
   end
 
   def new
     @equipment = Equipment.new
+    authorize @equipment
   end
 
   def create
-    @equipment = Equipment.new(equipment_params)
+    @equipment = Equipment.create(equipment_params)
     @equipment.user = current_user
+    authorize @equipment
+    if @equipment.save
+      redirect_to equipment_path(@equipment)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -27,7 +34,6 @@ class EquipmentController < ApplicationController
 
   def destroy
     @equipment.destroy
-    redirect_to
   end
 
   private
@@ -37,14 +43,6 @@ class EquipmentController < ApplicationController
   end
 
   def equipment_params
-    params.require(:equipment).permit[:name, :description, :price_per_day, :image, :category_id, :available]
-  end
-
-  def save_show_equipment
-    if @equipment.save
-      redirect_to equipment_path(@equipment)
-    else
-      render :new
-    end
+    params.require(:equipment).permit(:name, :description, :category_id)
   end
 end
