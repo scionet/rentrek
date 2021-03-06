0abdcb0618d8152aee3a1e92a6920caa0630305f
SLim <sam@samlim.com>
2021-03-05 21:38:57 -0500

    started validations

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,4 +1,9 @@
 class Equipment < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price_per_day, presence: true
+  validates :available, presence: true
 end
