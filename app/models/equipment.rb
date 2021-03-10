c2e6021fa0d2168ff896fd6004c24641628aa889
SLim <sam@samlim.com>
2021-03-09 19:44:02 -0500

    finish validation for all models

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,10 +1,11 @@
 class Equipment < ApplicationRecord
   has_many :reservations, dependent: :destroy
+  has_one_attached :photo
   belongs_to :user
   belongs_to :category
 
   validates :name, presence: true
-  validates :description, presence: true
-  validates :price_per_day, presence: true
+  validates :description, presence: true, length { maximum: 1000, too_long: "%{count} characters is the maximum allowed" }
+  validates :price_per_day, presence: true, numericality: true
   validates :available, presence: true
 end
