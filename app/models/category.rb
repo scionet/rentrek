826343fd0088094379b3e62be0a7c6ed1f92c7f0
Qiyun Dai <pianoquin@gmail.com>
2021-03-13 11:53:37 -0600

    The home page now features the newest 5 items. This is how the page will be for now. No carousel, no category list.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,12 @@
 class Category < ApplicationRecord
   has_many :equipment, dependent: :destroy
 
+  include PgSearch::Model
+  pg_search_scope :search_by_category,
+    against: [ :name ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
+  
   validates :name, presence: true
 end
